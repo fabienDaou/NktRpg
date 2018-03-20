3cf22034bb7ed9052cfa6b49ca62fd87220d603a
Fabien Daoulas <fabien.daoulas@gmail.com>
2018-03-20 18:06:02 +0100

    Add script to create NktRpg schema.

---
+++ src/main/NktRpgDatabase/CreateNktRpgSchema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE NktRpg;
+USE NktRpg;
+
+CREATE TABLE Session (id INT NOT NULL, date DATE NOT NULL, title VARCHAR(50), PRIMARY KEY(id));
+
+CREATE TABLE Event (
+    id INT NOT NULL,
+    session_id INT NOT NULL,
+    date DATE NOT NULL,
+    location VARCHAR(20),
+    description VARCHAR(500),
+    PRIMARY KEY(id),
+    FOREIGN KEY (session_id) REFERENCES Session(id)
+);

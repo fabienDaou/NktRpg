6fc63c209676dbf91f3a54c9d131a3652925413c
Fabien Daoulas <fabien.daoulas@gmail.com>
2018-03-20 17:17:45 +0100

    Add Event and Session classes.

---
+++ src/main/kotlin/NktRpg/Server/NktRpg/Event.kt
@@ -0,0 +1,5 @@
+package NktRpg.Server.NktRpg
+
+import java.time.LocalDateTime
+
+class Event(val date:LocalDateTime, val location:String, val description:String)
---
+++ src/main/kotlin/NktRpg/Server/NktRpg/Session.kt
@@ -0,0 +1,5 @@
+package NktRpg.Server.NktRpg
+
+import java.time.LocalDateTime
+
+class Session(val title:String, val date:LocalDateTime = LocalDateTime.now())

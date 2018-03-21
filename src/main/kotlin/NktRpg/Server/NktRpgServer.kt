b6e105a53aa3c96ecdc67f0dedbc82bdf749a42a
Fabien Daoulas <fabien.daoulas@gmail.com>
2018-03-21 22:08:06 +0100

    Use configvar in heroku to configure the server.

--- src/main/kotlin/NktRpg/Server/NktRpgServer.kt
+++ src/main/kotlin/NktRpg/Server/NktRpgServer.kt
@@ -11,5 +11,16 @@ fun main(args: Array<String>) {
         val sqlConnection = PostgreSqlConnection(args[1], args[2], args[3], args[4].toInt(), args[5])
         val store = NktRpgSqlStore(sqlConnection)
         embeddedServer(Netty, serverPort) { nktRpgServerModule(store) }.start()
+    } else {
+        val host = System.getenv()["DATABASE_HOST"]
+        val port = System.getenv()["DATABASE_PORT"]
+        val name = System.getenv()["DATABASE_NAME"]
+        val user = System.getenv()["DATABASE_USER"]
+        val password = System.getenv()["DATABASE_PASSWORD"]
+        val serverPort = System.getenv()["NKTRPG_SERVER_PORT"]
+
+        val sqlConnection = PostgreSqlConnection(user!!, password!!, host!!, port!!.toInt(), name!!)
+        val store = NktRpgSqlStore(sqlConnection)
+        embeddedServer(Netty, serverPort!!.toInt()) { nktRpgServerModule(store) }.start()
     }
 }

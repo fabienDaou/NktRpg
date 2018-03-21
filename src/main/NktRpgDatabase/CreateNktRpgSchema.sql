b09ed9aa36937455f753177cd1407a9c4d71a113
Maxime GILLET <gilet666@hotmail.fr>
2018-03-21 16:25:13 +0100

    extended model add tags, players...

--- src/main/NktRpgDatabase/CreateNktRpgSchema.sql
+++ src/main/NktRpgDatabase/CreateNktRpgSchema.sql
@@ -22,3 +22,37 @@ CREATE TABLE Comments (
     PRIMARY KEY(id),
     FOREIGN KEY (event_id) REFERENCES Events(id)
 );
+
+CREATE TABLE SessionPlayers (
+    id INT NOT NULL AUTO_INCREMENT,
+    session_id INT NOT NULL,
+    player_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (session_id) REFERENCES Sessions(id)
+    FOREIGN KEY (player_id) REFERENCES Players(id)
+);
+
+CREATE TABLE Players (
+    id INT NOT NULL AUTO_INCREMENT,
+    date DATETIME NOT NULL,
+    playername VARCHAR(20),
+    PRIMARY KEY(id),
+);
+
+CREATE TABLE Tags (
+    id INT NOT NULL AUTO_INCREMENT,
+    date DATETIME NOT NULL,
+    tag VARCHAR(20),
+    PRIMARY KEY(id),
+);
+
+CREATE TABLE EventTags (
+    id INT NOT NULL AUTO_INCREMENT,
+    event_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    date DATETIME NOT NULL,
+    tag VARCHAR(20),
+    PRIMARY KEY(id),
+    FOREIGN KEY (event_id) REFERENCES Events(id)
+    FOREIGN KEY (tag_id) REFERENCES Tags(id)
+);

807c3414fcb98d2794d689cbfe9ea8b320033470
Fabien Daoulas <fabien.daoulas@gmail.com>
2018-03-21 14:47:09 +0100

    Turn id property into a nullable int.

--- src/main/kotlin/NktRpg/Server/NktRpg/Event.kt
+++ src/main/kotlin/NktRpg/Server/NktRpg/Event.kt
@@ -1,15 +1,3 @@
 package NktRpg.Server.NktRpg
 
-data class Event(val location: String, val description: String, val date: Long = System.currentTimeMillis()){
-    var id: Int? = null
-        private set
-
-    var sessionId: Int? = null
-        private set
-
-    constructor(id: Int, sessionId: Int, location: String, description: String, date: Long)
-            : this(location, description, date){
-        this.id = id
-        this.sessionId = sessionId
-    }
-}
+data class Event(val id: Int?, val sessionId: Int?, val location: String, val description: String, val date: Long = System.currentTimeMillis())
--- src/main/kotlin/NktRpg/Server/NktRpg/Session.kt
+++ src/main/kotlin/NktRpg/Server/NktRpg/Session.kt
@@ -1,11 +1,3 @@
 package NktRpg.Server.NktRpg
 
-data class Session(val title: String, val date: Long = System.currentTimeMillis()){
-    var id: Int = 0
-        private set
-
-    constructor(id: Int, title: String, date: Long)
-            : this( title, date){
-        this.id = id
-    }
-}
+data class Session(val id: Int?, val title: String, val date: Long = System.currentTimeMillis())
